51b7dd43259c3744811db8de8b25d9ebf4d5c6c7
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2021-12-10 23:40:52 +0100

    added js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,25 @@
 'use strict';
+
+//Codigo de header
 // trayendome los elementos
 const btnElement = document.querySelector('.js-click');
 const menuElement = document.querySelector('.js-menu');
 
 function getHidden() {
-  if (menuElement.classList.contains('hidden')) {
-    menuElement.classList.toggle('hidden');
-  } else{
-    menuElement.classList.toggle('hidden');
-  }
+  menuElement.classList.toggle('hidden');
 }
 
 btnElement.addEventListener('click', getHidden);
+
+//collapsed Section Herramientas
+
+const collapsedElement = document.querySelector('.js-collapsed');
+
+function getClick(event) {
+  const currentTarget = event.currentTarget;
+  currentTarget.paternNode.classList.toggle('collapsed');
+}
+
+//listener
+
+collapsedElement.addEventListener('click', getClick);

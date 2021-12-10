156bb155843f9e01589af62ed6bb6ff309d21b85
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2021-12-10 22:26:54 +0100

    added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
+// trayendome los elementos
+const btnElement = document.querySelector('.js-click');
+const menuElement = document.querySelector('.js-menu');
 
-console.log('>> Ready :)');
+function getHidden() {
+  if (menuElement.classList.contains('hidden')) {
+    menuElement.classList.toggle('hidden');
+  } else{
+    menuElement.classList.toggle('hidden');
+  }
+}
+
+btnElement.addEventListener('click', getHidden);

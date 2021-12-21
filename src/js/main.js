aac26f6f0c5caacd6ba116a4151111aabffee0ad
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2021-12-21 19:50:50 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -13,10 +13,7 @@ btnElement.addEventListener('click', getHidden);
 
 //collapsed Section Herramientas
 
-const collapsedFrontElement = document.querySelector('.js-collapsed-front');
-const collapsedBackElement = document.querySelector('.js-collapsed-back');
-const collapsedOtherElement = document.querySelector('.js-collapsed-other');
-const collapsedSkillElement = document.querySelector('.js-collapsed-skill');
+const collapsedElement = document.querySelectorAll('.js-collapsed');
 
 function getClick(event) {
   const currentTarget = event.currentTarget;
@@ -25,7 +22,11 @@ function getClick(event) {
 
 //listener
 
-collapsedFrontElement.addEventListener('click', getClick);
-collapsedBackElement.addEventListener('click', getClick);
-collapsedOtherElement.addEventListener('click', getClick);
-collapsedSkillElement.addEventListener('click', getClick);
+function handlerAllElements(){
+  for(const elements of collapsedElement){
+    elements.addEventListener('click', getClick);
+  }
+}
+
+handlerAllElements();
+

8f0e97125c15dc4c6cfeefc893abe9e7d357ea89
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2022-01-04 16:02:38 +0100

    update changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,39 @@
-'use strict';
+"use strict";
 
 //Codigo de header
 // trayendome los elementos
-const btnElement = document.querySelector('.js-click');
-const menuElement = document.querySelector('.js-menu');
+const btnElement = document.querySelector(".js-click");
+const menuElement = document.querySelector(".js-menu");
 
 function getHidden() {
-  menuElement.classList.toggle('hidden');
+  menuElement.classList.toggle("hidden");
 }
 
-btnElement.addEventListener('click', getHidden);
+btnElement.addEventListener("click", getHidden);
 
 //collapsed Section Herramientas
 
-const collapsedElement = document.querySelectorAll('.js-collapsed');
+const collapsedFrontElement = document.querySelector(".js-collapsed-front");
+const collapsedBackElement = document.querySelector(".js-collapsed-back");
+const collapsedOtherElement = document.querySelector(".js-collapsed-other");
+const collapsedSkillElement = document.querySelector(".js-collapsed-skill");
 
 function getClick(event) {
   const currentTarget = event.currentTarget;
   currentTarget.parentNode.classList.toggle('collapsed');
+
+  const sectionElement = document.querySelectorAll('.js-section');
+
+  for (let index = 0; index < sectionElement.length; index++) {
+    if (!currentTarget.parentNode.classList.toggle("collapsed")) {
+      currentTarget.parentNode.classList.toggle("collapsed");
+    }
+  }
 }
 
 //listener
 
-function handlerAllElements(){
-  for(const elements of collapsedElement){
-    elements.addEventListener('click', getClick);
-  }
-}
-
-handlerAllElements();
-
+collapsedFrontElement.addEventListener("click", getClick);
+collapsedBackElement.addEventListener("click", getClick);
+collapsedOtherElement.addEventListener("click", getClick);
+collapsedSkillElement.addEventListener("click", getClick);

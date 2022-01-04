2dc60b566e3b43623565ee76269f9c4dcf4a9f5e
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2022-01-04 22:08:56 +0100

    add changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,23 @@
 "use strict";
 
-//Codigo de header
-// trayendome los elementos
-const btnElement = document.querySelector(".js-click");
-const menuElement = document.querySelector(".js-menu");
-
-function getHidden() {
-  menuElement.classList.toggle("hidden");
-}
-
-btnElement.addEventListener("click", getHidden);
-
 //collapsed Section Herramientas
-
-const collapsedFrontElement = document.querySelector(".js-collapsed-front");
-const collapsedBackElement = document.querySelector(".js-collapsed-back");
-const collapsedOtherElement = document.querySelector(".js-collapsed-other");
-const collapsedSkillElement = document.querySelector(".js-collapsed-skill");
 
 function getClick(event) {
   const currentTarget = event.currentTarget;
-  currentTarget.parentNode.classList.toggle('collapsed');
-
-  const sectionElement = document.querySelectorAll('.js-section');
-
-  for (let index = 0; index < sectionElement.length; index++) {
-    if (!currentTarget.parentNode.classList.toggle("collapsed")) {
-      currentTarget.parentNode.classList.toggle("collapsed");
-    }
+  currentTarget.parentNode.classList.toggle("collapsed");
+  document.getElementById('arrow').classList.toggle('rotate');
+  if(currentTarget.parentNode.classList.contains('collapsed')){
+currentTarget.parentNode.classList.add('collapsed');
   }
+
 }
 
 //listener
 
-collapsedFrontElement.addEventListener("click", getClick);
-collapsedBackElement.addEventListener("click", getClick);
-collapsedOtherElement.addEventListener("click", getClick);
-collapsedSkillElement.addEventListener("click", getClick);
+const listerEvent = () => {
+  const collapsedFront = document.querySelectorAll(".js-collapsed");
+  for (const element of collapsedFront) {
+    element.addEventListener("click", getClick);
+  }
+};
+listerEvent();
